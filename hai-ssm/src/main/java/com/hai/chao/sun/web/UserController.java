3efc2c8aae20e60fa32aa8d76b1faa1009ba126c
sunchaohai <sunchaohai@apass.cn>
2017-08-03 11:57:13 +0800

    通用页面访问方法。。

--- hai-ssm/src/main/java/com/hai/chao/sun/web/UserController.java
+++ hai-ssm/src/main/java/com/hai/chao/sun/web/UserController.java
@@ -2,6 +2,7 @@ package com.hai.chao.sun.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,19 +15,19 @@ import com.hai.chao.sun.vo.EasyUiPageResult;
 @Controller
 @RequestMapping("user")
 public class UserController {
-	@Autowired
-	private UserService userService;
-	
-	@RequestMapping("/page")
-	public ModelAndView toUsers() {
-		return new ModelAndView("users");
-	}
-	
-	@RequestMapping("/list")
-	@ResponseBody
-	public EasyUiPageResult listUser(@RequestParam(value="page",defaultValue="1")Integer pageNum,
-			@RequestParam(value="rows",defaultValue="5")Integer pageSize) {
-		return userService.querAllUser(pageNum,pageSize);
-	}
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping("/page/{pageName}")
+    public ModelAndView toUsers(@PathVariable("pageName") String pageName) {
+        return new ModelAndView(pageName);
+    }
+
+    @RequestMapping("/list")
+    @ResponseBody
+    public EasyUiPageResult listUser(@RequestParam(value = "page", defaultValue = "1") Integer pageNum,
+            @RequestParam(value = "rows", defaultValue = "5") Integer pageSize) {
+        return userService.querAllUser(pageNum, pageSize);
+    }
 
 }
