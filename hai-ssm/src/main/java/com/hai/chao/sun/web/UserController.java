686401f4e2961bf4c0bb640229cedba90c34d732
sunchaohai <1396543449@qq.com>
2017-08-06 20:01:14 +0800

    导出....

--- hai-ssm/src/main/java/com/hai/chao/sun/web/UserController.java
+++ hai-ssm/src/main/java/com/hai/chao/sun/web/UserController.java
@@ -1,19 +1,21 @@
 package com.hai.chao.sun.web;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.hai.chao.sun.pojo.User;
 import com.hai.chao.sun.service.UserService;
 import com.hai.chao.sun.vo.EasyUiPageResult;
 
 @Controller
-@RequestMapping("user")
+@RequestMapping("/user")
 public class UserController {
     @Autowired
     private UserService userService;
@@ -25,9 +27,21 @@ public class UserController {
 
     @RequestMapping("/list")
     @ResponseBody
-    public EasyUiPageResult listUser(@RequestParam(value = "page", defaultValue = "1") Integer pageNum,
+    public EasyUiPageResult<User> listUser(@RequestParam(value = "page", defaultValue = "1") Integer pageNum,
             @RequestParam(value = "rows", defaultValue = "5") Integer pageSize) {
         return userService.querAllUser(pageNum, pageSize);
+    }
+    
+    @RequestMapping("/export/excel")
+    @ResponseBody
+    public ModelAndView exportExcel(@RequestParam(value = "page", defaultValue = "1") Integer pageNum,
+            @RequestParam(value = "rows", defaultValue = "5") Integer pageSize) {
+        ModelAndView mv = new ModelAndView("excelView");
+        EasyUiPageResult<User> pageResult = userService.querAllUser(pageNum, pageSize);
+        List<User> users = pageResult.getRows();
+        mv.addObject("userList",users);
+        
+        return mv;
     }
 
 }
